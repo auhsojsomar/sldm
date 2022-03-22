a977083cca174c885f851f1d8e03acc0c7617406
Joshua Ramos <32316820+auhsojsomar@users.noreply.github.com>
2022-03-22 11:35:37 +0800

    add google analytics

---
+++ assets/js/gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K5P01PCRVN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K5P01PCRVN');
+</script>
